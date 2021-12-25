7a0849cc52a91fcd5b770517d178c3bc11bbbc8f
sharifhabiman <hdidiersharif@gmail.com>
2021-12-25 19:57:40 +0200

    audio changes

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,11 +1,24 @@
-import React,{useState} from 'react';
+/* eslint-disable no-unused-vars */
+import React,{useState,useEffect} from 'react';
 import {FiHeart} from 'react-icons/fi'
-import {FaPlay,FaHeart} from 'react-icons/fa'
+import {FaPlay,FaHeart,FaPause} from 'react-icons/fa'
 
 
-const Home = () => {
+
+const Home = ({song,play,status}) => {
     const[liked,setLiked]=useState(false);
     const[heart, setHeart]=useState(<FiHeart/>)
+    const[playing,setPlaying]=useState(status);
+    useEffect(() => {
+        setPlaying(status);
+        return undefined;
+    }, [status,playing])
+    const onclick= (e)=>{
+        e.preventDefault(); 
+        play(song.src);
+        
+      }
+
     const onlike = e => {
         e.preventDefault();
         if(!liked){
@@ -18,15 +31,16 @@ const Home = () => {
 
        
       }
-
+     
     return (
         <div className="song">
             <span className="p-btn" id="" >
-                <FaPlay/></span>
+                {playing?<FaPlay className="far" onClick={onclick} />:<FaPause className="far" onClick={onclick} />}
+               </span>
                 <span className="heart" onClick={onlike}>
                 {heart}
             </span>
-            <span className="song-name">Nice for What</span>
+            <span className="song-name">{song.name}</span>
             <span className="song-duration">1:20</span>
         </div>
     )
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,8 +5,7 @@ import { GrPause, GrPlay, GrChapterNext, GrChapterPrevious } from 'react-icons/g
 import { } from 'react-icons/im'
 import { FiRepeat, FiShuffle } from 'react-icons/fi'
 import Playlist from './Playlist';
-import sweeterman from '../assets/music/sweeterman.mp3'
-import {readdirSync} from 'fs'
+
 
 
 
@@ -14,17 +13,43 @@ const Home = () => {
     const [play, setPlay] = useState(false);
     let [playBtn, setPlayBtn] = useState(<GrPlay className="far" />)
     const[playStatus,setPlayStatus]=useState("initial")
-
-    const songs =readdirSync('./music');
-    console.log(songs);
-    
+    const [cur,setCur]=useState("00:00");
+     const [end,setEnd]=useState('00:00');
+    const importAll = r => r.keys().map(r);
+    const files = importAll(require.context('../assets/music/', false, /\.mp3$/));
+    const [song,setSong]=useState(files[0]);
+    const audiofiles=files.map((element)=>{
+        const array1=element.split('.');
+        const array2=array1[0].split('/');
+        const len=array2.length;
+        const name=array2[len-1]
+        if(song===element)
+        {return {
+            name,
+            src:element,
+        }}
+        else{
+            return {
+                name,
+                src:element,
+            } 
+        }
+    })
     useEffect(() => {
         const audio= document.getElementById('audio');
     const onplayed= document.getElementById("onplay");
     const  progressContainer =document.getElementById("progressContainer")
+    const  prev =document.getElementById("prev")
+    const  next =document.getElementById("next")
     function updateProgress(e){
         const {duration,currentTime}=e.srcElement;
+        const sec=Math.floor(((duration/60)%1)*100);
+        const min=Math.floor(duration/60);
+        setEnd(`${min}:${sec}`);
         const progressPercent =(currentTime/duration)*100;
+        const curSec=Math.floor(((currentTime/3600))*100);
+        const curMin=Math.floor(currentTime/60);
+        setCur(`${curMin}:${curSec}`)
         onplayed.style.width =`${progressPercent}%`;
  
     }
@@ -34,10 +59,38 @@ const Home = () => {
         const duration =audio.duration;
         audio.currentTime =(clickX/width)*duration;
     }
+    function nextSong(){
+        let songindex=files.indexOf(song);
+        songindex++
+        if(songindex>files.length-1){
+            songindex=0;
+        }
+        setSong(files[songindex]);
+        
+    }
+    function prevSong(){
+        let songindex=files.indexOf(song);
+        songindex--
+        if(songindex < 0){
+            songindex= files.length-1;
+        }
+        setSong(files[songindex]);
+        
+    }
+    prev.addEventListener('click',prevSong);
+    next.addEventListener('click',nextSong);
+    
         if(playStatus==='play'){
             
-            audio.play();
+            audio.play().catch((e)=>{
+                /* error handler */
+             })
+             ;
             audio.addEventListener('timeupdate',updateProgress);
+            audio.addEventListener('ended',nextSong);
+            prev.addEventListener('click',prevSong);
+            next.addEventListener('click',nextSong);
+            
         }
         if(playStatus==='pause'){
        onplayed.style.animationPlayState='paused'
@@ -45,7 +98,7 @@ const Home = () => {
         }
         progressContainer.addEventListener('click',setProgress);
         return undefined;
-    }, [playStatus])
+    }, [playStatus,song,files])
     const onplay = e => {
         e.preventDefault();
         if (!play) {
@@ -61,6 +114,9 @@ const Home = () => {
         }
     }
 
+    const changeAudioSrc=(src)=>{
+        setSong(src)
+    }
     return (
         <div className="container">
             <div className="row shadow-lg">
@@ -78,11 +134,10 @@ const Home = () => {
                         <span className="play-btn"><FaPlay className="fas" /></span>
                         <button>ADDPLAYLIST</button>
                         <div className="playlist-list" id="playlist-list">
-                            <Playlist />
-                            <Playlist />
-                            <Playlist />
-                            <Playlist />
-                            <Playlist />
+                            {audiofiles.map((element)=>(song===element.src?
+                            <Playlist song={element} play={changeAudioSrc}status={false}/>
+                            :<Playlist song={element} play={changeAudioSrc} status={true}/>))}
+
 
                         </div>
                     </div>
@@ -92,24 +147,23 @@ const Home = () => {
                                 <span className="controls-cover shadow"></span>
                                 <span className="controls-name">Nice For What</span>
                             </div>
-                            <audio src="./audio/Music/24kgoldn_mood.mp3" id="24k"></audio>
                             <div className="col-3 controls-btn">
                                 <span><FiShuffle className="far" /></span>
-                                <span><GrChapterPrevious className="far" /></span>
+                                <span><GrChapterPrevious className="far" id="prev" /></span>
                                 <span onClick={onplay} class="far fa-play" id="paused"   >{playBtn}</span>
-                                <span><GrChapterNext className="far" /></span>
+                                <span><GrChapterNext className="far" id="next"/></span>
                                 <span><FiRepeat className="far" /></span>
                             </div>
                             <div className="col-6" >
-                                <span className="start">00:00</span> <span className="play-line" id="progressContainer">
+                                <span className="start">{cur}</span> <span className="play-line" id="progressContainer">
             
                                         <div className="onplay" id="onplay">
 
                                             <BsCircle className="far" /></div>
 
-                                </span> <span>01:20</span>
+                                </span> <span>{end}</span>
                             </div>
-                            <audio src={sweeterman} id="audio"></audio>
+                            <audio src={song} id="audio"></audio>
                         </div>
                     </div>
                 </div>
